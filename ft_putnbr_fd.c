aa5864a95ceb1669a944c111f25ae42750be1d96
kdavis <kyweda@gmail.com>
2016-11-02 21:19:40 -0700

    simplified ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 10:52:53 by kdavis            #+#    #+#             */
-/*   Updated: 2016/11/02 21:11:22 by kdavis           ###   ########.fr       */
+/*   Updated: 2016/11/02 21:18:49 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,10 @@ void	ft_putnbr_fd(int n, int fd)
 	sign = 1;
 	if (n < 0)
 	{
-		if (n < -9)
-			sign = -1;
-		else
-			n = -n;
+		sign = -1;
 		ft_putchar_fd('-', fd);
 	}
-	if (n > 9 || n < 0)
+	if (n > 9 || n < -9)
 	{
 		ft_putnbr_fd(((n / 10) * sign), fd);
 		ft_putnbr_fd(((n % 10) * sign), fd);
